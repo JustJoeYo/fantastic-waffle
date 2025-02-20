f5f4198ebdff8d5e7e5f77b1c90560234c58ad17
Joe <53631725+JustJoeYo@users.noreply.github.com>
2025-02-20 08:10:35 -0800

    initial push

--- spec/passenger_spec.rb
+++ spec/passenger_spec.rb
@@ -1,4 +1,4 @@
-require './lib/passenger'
+require './spec_helper.rb'
 
 # This block helps format the test results
 RSpec.configure do |config|
@@ -6,6 +6,35 @@ RSpec.configure do |config|
 end
 
 # Your tests should start here!
-RSpec.describe Passenger do 
-    
+RSpec.describe Passenger do
+  before(:each) do
+    @charlie = Passenger.new({"name" => "Charlie", "age" => 18})
+    @taylor = Passenger.new({"name" => "Taylor", "age" => 12})
+  end
+
+  it 'initialize' do
+    expect(@charlie).to be_a(Passenger)
+    expect(@taylor).to be_a(Passenger)
+  end
+
+  it 'has name' do
+    expect(@charlie.name).to eq("Charlie")
+    expect(@taylor.name).to eq("Taylor")
+  end
+
+  it 'has age' do
+    expect(@charlie.age).to eq(18)
+    expect(@taylor.age).to eq(12)
+  end
+
+  it 'check if passenger is an adult' do
+    expect(@charlie.adult?).to be true
+    expect(@taylor.adult?).to be false
+  end
+
+  it 'checks if passenger can drive' do
+    expect(@charlie.driver?).to be false
+    @charlie.drive
+    expect(@charlie.driver?).to be true
+  end
 end
